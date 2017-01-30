ff01619a6bb2bc84927d478fb64ecdcbc357b0ac
Kirill <vorobjovkirill@gmail.com>
2017-01-30 17:32:34 +0100

    Test
    Test Search

---
+++ AnimeSearch/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace AnimeSearch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbSearch = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lvAnimeResult = new System.Windows.Forms.ListView();
+            this.tbUserID = new System.Windows.Forms.TextBox();
+            this.tbPass = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbSearch
+            // 
+            this.tbSearch.Location = new System.Drawing.Point(74, 12);
+            this.tbSearch.Name = "tbSearch";
+            this.tbSearch.Size = new System.Drawing.Size(206, 20);
+            this.tbSearch.TabIndex = 0;
+            this.tbSearch.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbSearch_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Search :";
+            // 
+            // lvAnimeResult
+            // 
+            this.lvAnimeResult.Location = new System.Drawing.Point(15, 96);
+            this.lvAnimeResult.Name = "lvAnimeResult";
+            this.lvAnimeResult.Size = new System.Drawing.Size(566, 360);
+            this.lvAnimeResult.TabIndex = 2;
+            this.lvAnimeResult.UseCompatibleStateImageBehavior = false;
+            // 
+            // tbUserID
+            // 
+            this.tbUserID.Location = new System.Drawing.Point(481, 31);
+            this.tbUserID.Name = "tbUserID";
+            this.tbUserID.Size = new System.Drawing.Size(100, 20);
+            this.tbUserID.TabIndex = 3;
+            // 
+            // tbPass
+            // 
+            this.tbPass.Location = new System.Drawing.Point(481, 70);
+            this.tbPass.Name = "tbPass";
+            this.tbPass.Size = new System.Drawing.Size(100, 20);
+            this.tbPass.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(478, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Username :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(478, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Password :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 470);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbPass);
+            this.Controls.Add(this.tbUserID);
+            this.Controls.Add(this.lvAnimeResult);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbSearch);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbSearch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView lvAnimeResult;
+        private System.Windows.Forms.TextBox tbUserID;
+        private System.Windows.Forms.TextBox tbPass;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ AnimeSearch/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MyAnimeListSharp;
+using MyAnimeListSharp.Core;
+using MyAnimeListSharp.Auth;
+using MyAnimeListSharp.Util;
+using MyAnimeListSharp.Facade;
+using MyAnimeListSharp.Parameters;
+using MyAnimeListSharp.Facade.Async;
+using MyAnimeListSharp.Extensions.Core;
+
+namespace AnimeSearch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Create Listview Columns
+            lvAnimeResult.View = View.Details;
+
+            lvAnimeResult.Columns.Add("Anime", 382);
+            lvAnimeResult.Columns.Add("Type", 50);
+            lvAnimeResult.Columns.Add("Episodes", 70);
+            lvAnimeResult.Columns.Add("Rating", 70);
+        }
+
+        private async void tbSearch_KeyDown(object sender, KeyEventArgs e)
+        {
+            ICredentialContext credential = new CredentialContext
+            {
+                UserName = tbUserID.Text,
+                Password = tbPass.Text
+            };
+
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (string.IsNullOrEmpty(tbSearch.Text))
+                {
+                    MessageBox.Show("Nothing to Search for.....");
+                }
+                else
+                {
+                    string response;
+
+                    var asyncAnimeSearch = new AnimeSearchMethodsAsync(credential);
+                    response = await asyncAnimeSearch.SearchAsync(tbSearch.Text);
+
+                    if (string.IsNullOrEmpty(tbUserID.Text) &&  string.IsNullOrEmpty(tbPass.Text))
+                    {
+                        MessageBox.Show("Add account");
+                    }
+                    else if (string.IsNullOrWhiteSpace(response))
+                    {
+                        MessageBox.Show("No such anime");
+                    }
+                    else
+                    {
+                        lvAnimeResult.BeginUpdate();
+                        lvAnimeResult.Items.Clear();
+
+                        XmlDocument doc = new XmlDocument();
+
+                        doc.LoadXml(response);
+
+                        XmlNodeList list = doc.SelectNodes("/anime/entry");
+
+                        foreach (XmlNode node in list)
+                        {
+                            string series_id = node["id"].InnerText;
+                            string series_title = node["title"].InnerText;
+                            string series_title_english = node["english"].InnerText;
+                            string series_episodes = node["episodes"].InnerText;
+                            string series_score = node["score"].InnerText;
+                            string series_type = node["type"].InnerText;
+                            string series_status = node["status"].InnerText;
+                            string series_start_date = node["start_date"].InnerText;
+                            string series_end_date = node["end_date"].InnerText;
+                            string series_image = node["image"].InnerText;
+
+                            string[] lvElements = new string[11];
+                            ListViewItem items;
+
+                            lvElements[0] = series_title;
+                            lvElements[1] = series_type;
+                            lvElements[2] = series_episodes;
+                            lvElements[3] = series_score;
+                            lvElements[4] = series_id;
+                            lvElements[5] = series_title_english;
+                            lvElements[6] = series_status;
+                            lvElements[7] = series_start_date;
+                            lvElements[8] = series_end_date;
+                            lvElements[9] = series_image;
+
+                            items = new ListViewItem(lvElements);
+                            lvAnimeResult.Items.Add(items);
+                            lvAnimeResult.EndUpdate();
+                        }
+                    }
+                }
+            }
+        }        
+    }
+}
