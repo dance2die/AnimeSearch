5e8d56e73465714124169b7632959f31bb30424a
dance2die <dontbothermewithspam@gmail.com>
2017-02-22 09:45:46 -0500

    refactored code.

--- UpdateAnimeValueConsole/Program.cs
+++ UpdateAnimeValueConsole/Program.cs
@@ -3,7 +3,6 @@ using MyAnimeListSharp.Auth;
 using MyAnimeListSharp.Core;
 using MyAnimeListSharp.Enums;
 using MyAnimeListSharp.Facade;
-using MyAnimeListSharp.Util;
 
 namespace UpdateAnimeValueConsole
 {
@@ -19,52 +18,15 @@ namespace UpdateAnimeValueConsole
         {
             var animeId = 32615;    // Youjo Senki (TV)
             short epsWatched = 6;
-            var persScore = 10;
+            var persScore = 8;
 
             var methods = new AnimeListMethods(credential);
 
-            AnimeValues animeValues = new AnimeValues {Episode = epsWatched};
-
-            if (persScore == 0 || persScore == 1)
+            AnimeValues animeValues = new AnimeValues
             {
-                animeValues.Score = Score.Appalling;
-            }
-            else if (persScore == 2)
-            {
-                animeValues.Score = Score.Horrible;
-            }
-            else if (persScore == 3)
-            {
-                animeValues.Score = Score.VeryBad;
-            }
-            else if (persScore == 4)
-            {
-                animeValues.Score = Score.Bad;
-            }
-            else if (persScore == 5)
-            {
-                animeValues.Score = Score.Average;
-            }
-            else if (persScore == 6)
-            {
-                animeValues.Score = Score.Fine;
-            }
-            else if (persScore == 7)
-            {
-                animeValues.Score = Score.Good;
-            }
-            else if (persScore == 8)
-            {
-                animeValues.Score = Score.VeryGood;
-            }
-            else if (persScore == 9)
-            {
-                animeValues.Score = Score.Great;
-            }
-            else if (persScore == 10)
-            {
-                animeValues.Score = Score.MasterPiece;
-            }
+                Episode = epsWatched,
+                Score = (Score)persScore
+            };
 
             var response = methods.UpdateAnime(animeId, animeValues);
             Console.WriteLine(response);
